623d5409325bcefbc297a4b0275e986d85cbb96f
yaxley-peaks <epiclycoolgaemer@gmail.com>
2022-12-06 20:50:54 +0530

    divvy

--- day6/main.hs
+++ day6/main.hs
@@ -1,11 +1,13 @@
-import Data.List ( nub, tails )
-
+import Data.List (findIndex, nub, tails)
+import Data.List.Split (divvy)
 
 windows' :: Int -> [a] -> [[a]]
 windows' n xs = map (take n) $ tails xs
 
 windows :: Int -> [a] -> [[a]]
 windows n xs = take (length xs - n + 1) (windows' n xs)
+
+solve' n xs = fmap (+ 1) $ findIndex (\x -> nub x == x) $ divvy n 1 xs
 
 solve :: Int -> String -> Int
 solve n xs =
@@ -18,5 +20,5 @@ solve n xs =
 main :: IO ()
 main = do
   contents <- readFile "./inp.txt"
-  print $ solve 4 contents
+  print $ solve' 4 contents
   print $ solve 14 contents
